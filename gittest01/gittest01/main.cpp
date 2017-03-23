d0093138de74cb5567df5d0854ef424ae376650a
ohno keisuke <tejastice@yahoo.co.jp>
2017-03-23 18:12:01 +0900

    ファイルを追加

--- gittest01/gittest01/main.cpp
+++ gittest01/gittest01/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
 
 	std::cout << "hello GIT!" << std::endl;
 
 	//testkomment
+
+	std::fstream file;
+	file.open("Saaaa.csv", std::ios::out);
+
 
 }
